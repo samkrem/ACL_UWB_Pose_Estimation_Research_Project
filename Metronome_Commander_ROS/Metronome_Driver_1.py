eb88ec6e05c553e6ccd245707598a284689c49c0
Sammy Krem <93107379+samkrem@users.noreply.github.com>
2024-05-29 22:44:38 -0400

    Updated ROS code and add Waypoint Scheduler

--- Metronome_Commander_ROS/Metronome_Driver_1.py
+++ Metronome_Commander_ROS/Metronome_Driver_1.py
@@ -2,22 +2,23 @@
 import serial
 import rospy
 import time
-from std_msgs.msg import Float32
+from std_msgs.msg import Float32MultiArray
 from std_msgs.msg import Int16
 
 
 class MetronomeDriver1:
     def __init__(self):
-        rospy.init_node("MetronomeDriver1")
-        self.target_angle_sub = rospy.Subscriber(
-            "target_angle", Int16, callback=self.target_angle_callback
+        rospy.init_node("MetronomeDriver2")
+        self.az_el_angle_sub = rospy.Subscriber(
+            "az_el_angle_2", Float32MultiArray, callback=self.az_el_angle_callback
         )
         self.arduino_port = serial.Serial("/dev/ttyACM0", baudrate=1000000, timeout=1)
 
-    def target_angle_callback(self, msg: Int16):
-        target_angle_bytes = bytes(str(msg.data), "utf-8")
-        rospy.loginfo("Master received " + str(msg.data) + "°. Sent to Arduino.")
-        self.arduino_port.write(target_angle_bytes)
+    def az_el_angle_callback(self, msg: Float32MultiArray):
+        az, el = msg.data
+        az_el_angle_bytes = bytes(f"({az},{el})", "utf-8")
+        rospy.loginfo(f"Master received ({round(az)}°, {round(el)}°). Sent to Arduino.")
+        self.arduino_port.write(az_el_angle_bytes)
         time.sleep(0.5)
         arduino_response = self.arduino_port.readline()
         rospy.loginfo(arduino_response)
